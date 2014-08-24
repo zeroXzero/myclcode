440b65f5cd8cef9b1ad47672a140934c7a26dfb4
Sreeraj Rajendran <rsreeraj@gmail.com>
2014-08-24 18:08:51 +0530

    Added more stopwords

--- cloud/main.js
+++ cloud/main.js
@@ -89,7 +89,19 @@ Parse.Cloud.beforeSave("Question", function(request, response) {
 
 		var words = post.get("data").split(/\b/);
 		words = _.map(words, toLowerCase);
-		var stopWords = ["the", "in", "and", "why", "how", "what", "when", "or", "where", "is", "was", "were", "are"]
+		var stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+						 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+						 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+						 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+						 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+						 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+						 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+						 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+						 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+						 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+						 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+						 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+
 			words = _.filter(words, function(w) { return w.match(/^\w+$/) && ! _.contains(stopWords, w); });
 
 		var hashtags = post.get("data").match(/#.+?\b/g);
